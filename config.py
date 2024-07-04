1d509fa370b0cfe63d3d40f10cf7106383620109
actchanthar <159926023+actchanthar@users.noreply.github.com>
2024-07-04 17:45:12 +0630

    Add files via upload

--- config.py
+++ config.py
@@ -5,22 +5,22 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", "")  # ⚠️ Required
-    API_HASH  = os.environ.get("API_HASH", "") # ⚠️ Required
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") # ⚠️ Required
-    FORCE_SUB = os.environ.get('FORCE_SUB', '') # ⚠️ Required
+    API_ID    = os.environ.get("API_ID", "28090485")  # ⚠️ Required
+    API_HASH  = os.environ.get("API_HASH", "d2cbcc3d47fdb27530eb4f29fbe80534") # ⚠️ Required
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7327859748:AAH9wUzPFJLKuT63DHV1aXQ-EVEKOeTQpog") # ⚠️ Required
+    FORCE_SUB = os.environ.get('FORCE_SUB', '-1001760659516') # ⚠️ Required
     AUTH_CHANNEL = int(FORCE_SUB) if FORCE_SUB and id_pattern.search(
     FORCE_SUB) else None
    
     # database config
-    DB_URL  = os.environ.get("DB_URL", "")  # ⚠️ Required
+    DB_URL  = os.environ.get("DB_URL", "mongodb+srv://actnat:actnat@cluster0.hmlrpgv.mongodb.net/?retryWrites=true&w=majority")  # ⚠️ Required
     DB_NAME  = os.environ.get("DB_NAME","SnowEncoderBot") 
 
     # Other Configs 
-    ADMIN = int(os.environ.get("ADMIN", "0")) # ⚠️ Required
-    LOG_CHANNEL = int(os.environ.get('LOG_CHANNEL', '0')) # ⚠️ Required
+    ADMIN = int(os.environ.get("ADMIN", "5062124930")) # ⚠️ Required
+    LOG_CHANNEL = int(os.environ.get('LOG_CHANNEL', '-1002005922497')) # ⚠️ Required
     BOT_UPTIME = BOT_UPTIME  = time.time()
-    START_PIC = os.environ.get("START_PIC", "https://graph.org/file/15e82d7e665eccc8bd9c5.jpg")
+    START_PIC = os.environ.get("START_PIC", "https://telegra.ph/file/3392347510ff67bb7eda2.jpg")
 
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
