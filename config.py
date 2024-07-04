182bfe316744803a6c6a48d588e6f01af7bb0f74
actchanthar <159926023+actchanthar@users.noreply.github.com>
2024-07-04 19:59:54 +0630

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     # pyro client config
     API_ID    = os.environ.get("API_ID", "28090485")  # ⚠️ Required
     API_HASH  = os.environ.get("API_HASH", "d2cbcc3d47fdb27530eb4f29fbe80534") # ⚠️ Required
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7327859748:AAH9wUzPFJLKuT63DHV1aXQ-EVEKOeTQpog") # ⚠️ Required
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7327859748:AAERr392isEIjofiCOwC2BfeCGPx0HJkICY") # ⚠️ Required
     FORCE_SUB = os.environ.get('FORCE_SUB', '-1001760659516') # ⚠️ Required
     AUTH_CHANNEL = int(FORCE_SUB) if FORCE_SUB and id_pattern.search(
     FORCE_SUB) else None
@@ -20,7 +20,7 @@ class Config(object):
     ADMIN = int(os.environ.get("ADMIN", "5062124930")) # ⚠️ Required
     LOG_CHANNEL = int(os.environ.get('LOG_CHANNEL', '-1002005922497')) # ⚠️ Required
     BOT_UPTIME = BOT_UPTIME  = time.time()
-    START_PIC = os.environ.get("START_PIC", "https://telegra.ph/file/3392347510ff67bb7eda2.jpg")
+    START_PIC = os.environ.get("START_PIC", "")
 
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
